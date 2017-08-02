c96901ac41197e151e755c5555dce813ac21a9c4
ColtonSmith28 <coltonsmith28@gmail.com>
2017-08-02 18:37:27 -0400

    dave helps commit

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,8 +2,8 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-
-
+for(var counter = 0; counter<=100; counter= counter + 1)
+console.log(counter);
 
 /*
 Exercise #2
@@ -17,3 +17,9 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var triangle = "#"
+while (triangle.length <= 7) {
+  console.log(triangle);
+  triangle = triangle + "#";
+}
